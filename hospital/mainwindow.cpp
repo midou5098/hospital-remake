59422d8da4dbe31df6bd4c169cdbd3e498278447
mohamed amdouni <midouamdouni4@gmail.com>
2026-05-31 12:15:26 +0000

    more pages linked and added the full sql database (4 tables with various details)

--- hospital/mainwindow.cpp
+++ hospital/mainwindow.cpp
@@ -14,6 +14,11 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->register_but,&QPushButton::clicked,this,[this]() { switchpg(1); });
     connect(ui->regist_but,&QPushButton::clicked,this,[this]() { switchpg(0); });
     connect(ui->logout,&QPushButton::clicked,this,[this]() { switchpg(0); });
+    connect(ui->add_d,&QPushButton::clicked,this,[this]() { switchpg(3); });
+    connect(ui->back,&QPushButton::clicked,this,[this]() { switchpg(2); });
+    connect(ui->add_n,&QPushButton::clicked,this,[this]() { switchpg(4); });
+    connect(ui->back_2,&QPushButton::clicked,this,[this]() { switchpg(2); });
+
     ui->centralwidget->setLayout(layout);
 }
 // the layout : 0=login screen,1 register,2 main menu,3 is used for the nurse section(31,32,33 just like the old systems) ,4 for the doctors , and 5 for the patients , and -1 for the listing page
