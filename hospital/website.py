981efa8f328d1fd1bc204aec2d8f32e7284ac9ae
mohamed amdouni <midouamdouni4@gmail.com>
2026-06-05 10:33:52 +0000

    created the python server(used the SimpleHTTPRequestHandler since it's just a small feature)

---
+++ hospital/website.py
@@ -0,0 +1,27 @@
+import json
+import os
+from http.server import HTTPServer,SimpleHTTPRequestHandler
+from urllib.parse import urlparse
+
+
+class handler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        parsed=self.path
+        if parsed=="/records":
+            self.send_response(200)
+            self.send_header("Content-type","application/json")
+            self.send_header("Cache-Control","no-Cache,must-revalidate")
+            self.end_headers()
+            with open("data.json","r",encoding="utf_8")as f:
+                data=json.load(f)
+            self.wfile.write(json.dumps(data).encode())
+
+
+def run_tha_server_twin(port=8080):
+    server_adress=("",port)
+    httpd=HTTPServer(server_adress,handler)
+    print(f"the server is running in god knows where.(http://localhost:{port})")
+    httpd.serve_forever()
+
+if __name__== "__main__":
+    run_tha_server_twin();
